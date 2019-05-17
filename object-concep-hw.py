b48c5b11e0cb4b5df6fb2f31d57e9bcbb9f46326
dmytromoiseienko <dmytro.moiseienko@gmail.com>
2019-05-18 02:44:01 +0300

    update HW object-concept

--- object-concep-hw.py
+++ object-concep-hw.py
@@ -7,69 +7,70 @@ from constants import *
 class Car:
 
     def __init__(self, price, type, producer, number, mileage):
-        """ initialization attributes class Car """
+        """ initialization attributes class Car and check it on type """
         self.price = price
         self.type = type
         self.producer = producer
         self.number = number
         self.mileage = mileage
+        assert isinstance(price, float), 'type price expected "float"'
+        assert type in CARS_TYPES, 'choose type from CARS_TYPES'
+        assert producer in CARS_PRODUCER, 'choose producer from CARS_PRODUCER'
+        assert isinstance(number, uuid.UUID), 'type number expected "uuid"'
+        assert isinstance(mileage, float), 'type mileage expected "float"'
 
     def change_number(self):
         """ change car number """
         self.number = uuid.uuid4()
 
-    def expensive(self, *args):
-        """ display cars from more expansive to cheaper """
-        cont_cars = {}
-        for i in args:
-            cont_cars[i.price] = '{} with number {}'.format(i.producer, i.number)
-        cont_cars = sorted(cont_cars.items(), key=lambda kv: kv[0])
-        [print('{}$ for {}'.format(i[0], i[1])) for i in reversed(cont_cars)]
+    def compare(self, other):
+        """ compare prices cars and display it """
+        if self.__eq__(other):
+            print(f'price car {self.producer} == {other.producer}')
+        elif self.__lt__(other):
+            print(f'price car {self.producer} < {other.producer}')
+        elif self.__ge__(other):
+            print(f'price car {self.producer} > {other.producer}')
 
-    def cheaper(self, *args):
-        """ display cars from more cheaper to more expensive """
-        cont_cars = {}
-        for i in args:
-            cont_cars[i.price] = '{} with number {}'.format(i.producer, i.number)
-        cont_cars = sorted(cont_cars.items(), key=lambda kv: kv[0])
-        [print('{}$ for {}'.format(i[0], i[1])) for i in cont_cars]
+    def __lt__(self, other):
+        return self.price < other.price
 
-    def __repr__(self):
-        """ display attributes class Car """
-        # print('вызвал repr')
-        return 'Car atributes: ' \
-               'price - {}$, ' \
-               'type - {}, ' \
-               'producer - {}, ' \
-               'number - {}, ' \
-               'mileage - {}'.format(self.price, self.type, self.producer, self.number, self.mileage)
+    def __eq__(self, other):
+        return self.price == other.price
+
+    def __ge__(self, other):
+        return self.price >= other.price
 
     def __str__(self):
-        # print('вызвал str')
-        return self.__repr__()
+        return f'Object of class Car() has 5 attributes: self.price - {self.price}$\n' \
+            f'{" " * 40}self.type - {self.type}\n' \
+            f'{" " * 40}self.producer - {self.producer}\n' \
+            f'{" " * 40}self.number - {self.number}\n' \
+            f'{" " * 40}self.mileage - {self.mileage}\n'
 
 
 class Garage:
 
     def __init__(self, town, cars, places, owner=None):
-        """ initialization attributes class Garage """
+        """ initialization attributes class Garage and check it on type """
         self.town = town
         self.cars = cars
         self.places = places
         self.owner = owner
-        if len(self.cars) > self.places:
-            print('Garage can include only {} car\nPLS reload Garage'.format(self.places))
+        assert town in TOWNS, 'choose town from TOWNS'
+        assert isinstance(cars, list), 'type cars expected "list"'
+        assert isinstance(places, int), 'type places expected "int"'
+        assert self.__le__(places), f'Garage can include only {self.places} car\n' \
+            f'{" " * 16}PLS reload Garage'
+        if owner:
+            assert isinstance(places, uuid.UUID), 'type owner expected "uuid"'
 
     def hit_hat(self):
-        """ Sum all prices cars in Garage and display it """
-        sum = 0
-        for i in self.cars:
-            sum += i.price
-        # print('Total price of all cars in garage: {}$'.format(sum))
-        return sum
+        """ display total price all cars in garage """
+        print(f'Total price of all cars in garage: {self._sum_prices()}$')
 
     def add(self, new_car):
-        """ Add car to Garage """
+        """ Add car to Garage if it possible """
         if len(self.cars) < self.places:
             self.cars.append(new_car)
             print('Successful add new car to Garage')
@@ -77,88 +78,133 @@ class Garage:
             return 'No free place'
 
     def remove(self, old_car):
-        """ Remove car from Garage """
+        """ Remove car from Garage if it possible """
         if old_car in self.cars:
             self.cars.remove(old_car)
             print('Successful remove car from Garage')
         else:
             print('No such car in the Garage')
 
-    def __repr__(self):
-        return 'This is class Garage' \
-               'you can use dir() to see it attributes'
+    def _sum_prices(self):
+        """ Sum all prices cars in Garage """
+        return sum([i.price for i in self.cars])
+
+    def __le__(self, other):
+        return len(self.cars) <= other
 
     def __str__(self):
-        # print('вызвал str')
-        return self.__repr__()
+        return 'This is class Garage' \
+               'you can use dir() to see it attributes'
 
 
 class Cesar:
 
     def __init__(self, name, garages, register_id):
-        """ initialization attributes class Garage """
+        """ initialization attributes class Garage and check it on type """
         self.name = name
         self.garages = garages
         self.register_id = register_id
+        assert isinstance(name, str), 'type name expected "str"'
+        assert isinstance(garages, list), 'type garages expected "list"'
+        assert isinstance(register_id, uuid.UUID), 'type register_id expected "uuid"'
 
     def hit_hat(self):
-        """ Sum all prices cars in all garages and display it """
-        sum_all_cars = 0
-        for i in self.garages:
-            sum_all_cars += i.hit_hat()
-        # print('Total price of all cars in all garages: {}$'.format(sum_all_cars))
-        return sum_all_cars
+        """ Display total price of all cars in all garages """
+        print(f'Total price of all cars in all garages: {self._sum_prices()}$')
 
     def garage_count(self):
-        """ Sum all count garages and display it """
+        """ Display total count of garages """
         print('Total count of garages: {}'.format(len(self.garages)))
 
     def cars_count(self):
-        """ Sum all count cars in all garages and display it """
-        sum_count = 0
-        for i in self.garages:
-            sum_count += len(i.cars)
-        print('Total count of cars: {}'.format(sum_count))
+        """ Display total cars in all garages """
+        print(f'Amount cars in all garages: {sum([len(i.cars) for i in self.garages])}')
 
     def add_car(self, new_car, chosen_garage):
-        """ Add car to garage if it possibly """
+        """ Add car to garage if it possible """
         if chosen_garage.add(new_car) != 'No free place':
             chosen_garage.add(new_car)
-        elif self.free_garage() != 'No free place':
-            self.free_garage().add(new_car)
+        elif self._free_garage() != 'No free place':
+            self._free_garage().add(new_car)
         else:
             print('No free place')
 
-    def free_garage(self):
+    def compare(self, other):
+        """ compare cesar's """
+        if self.__eq__(other):
+            print(f'Cesar {self.name} and {other.name} are equal')
+        elif self.__lt__(other):
+            print(f'Cesar {self.name} less richer then {other.name}')
+        elif self.__ge__(other):
+            print(f'Cesar {self.name} richer {other.name}')
+
+    def __lt__(self, other):
+        return self._sum_prices() < other._sum_prices()
+
+    def __eq__(self, other):
+        return self._sum_prices() == other._sum_prices()
+
+    def __ge__(self, other):
+        return self._sum_prices() >= other._sum_prices()
+
+    def _free_garage(self):
         """ looking for more free garage """
         more_free = cesar.garages[0]
         for i in cesar.garages:
-            if len(i.cars) < len(more_free.cars):
+            if (i.places - len(i.cars)) > (i.places - len(more_free.cars)):
                 more_free = i
         if len(more_free.cars) == more_free.places:
             return 'No free place'
         else:
             return more_free
 
-    def expensive(self, *args):
-        """ display cesar from richest to poorer """
-        price_cars = {}
-        for i in args:
-            price_cars[i.hit_hat()] = i.name
-        price_cars = sorted(price_cars.items(), key=lambda kv: kv[0])
-        [print('{}$ total price cars of cesar - {}'.format(i[0], i[1])) for i in reversed(price_cars)]
-
-    def cheaper(self, *args):
-        """ display cesar from poorer to richest """
-        price_cars = {}
-        for i in args:
-            price_cars[i.hit_hat()] = i.name
-        price_cars = sorted(price_cars.items(), key=lambda kv: kv[0])
-        [print('{}$ total price cars of cesar - {}'.format(i[0], i[1])) for i in price_cars]
-
-    def __repr__(self):
-        pass
+    def _sum_prices(self):
+        """ Sum all prices cars in all garages """
+        return sum([i._sum_prices() for i in self.garages])
 
     def __str__(self):
-        # print('вызвал str')
-        return self.__repr__()
+        return 'This is class Cesar' \
+               'you can use dir() to see it attributes'
+
+
+if __name__ == '__main__':
+    car = Car(float(random.choice(range(20000, 50000))),
+              random.choice(CARS_TYPES),
+              random.choice(CARS_PRODUCER),
+              uuid.uuid4(),
+              float(random.choice(range(3000, 15000)))
+              )
+    car1 = Car(float(random.choice(range(20000, 50000))),
+               random.choice(CARS_TYPES),
+               random.choice(CARS_PRODUCER),
+               uuid.uuid4(),
+               float(random.choice(range(3000, 15000)))
+               )
+    # car.compare(car1)
+    # print(car)
+    cont_cars = [car]
+    garage = Garage(random.choice(TOWNS),
+                    cont_cars,
+                    2,
+                    )
+    cont_cars = [car1]
+    garage1 = Garage(random.choice(TOWNS),
+                     cont_cars,
+                     1,
+                     )
+    # garage.hit_hat()
+    cont_garage = [garage, garage1]
+    cesar = Cesar('I am',
+                  cont_garage,
+                  uuid.uuid4()
+                  )
+    cont_garage = [garage]
+    cesar1 = Cesar('Hi',
+                   cont_garage,
+                   uuid.uuid4()
+                   )
+    cesar.compare(cesar1)
+    # cesar.hit_hat()
+    # cesar.cars_count()
+    # cesar.add_car(car, garage)
+    # print(cesar.garage_count().doc)
